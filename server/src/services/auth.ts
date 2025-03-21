3134b9c6059c15f6c2d7bcd555cdf35e0f51ce48
Jacob Wilson <wilson.jacob62@gmail.com>
2025-03-21 13:00:14 -0600

    updates

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,31 +1,51 @@
-
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { Request, Response, NextFunction } from 'express';
 
 dotenv.config();
 
 interface JwtPayload {
   _id: unknown;
   username: string;
-  email: string,
+  email: string;
 }
 
 export const getUser = (token: string | undefined): JwtPayload | null => {
-  if(!token) return null;
+  if (!token) return null;
 
   const secretKey = process.env.JWT_SECRET_KEY || '';
+
   try {
     const decoded = jwt.verify(token, secretKey) as JwtPayload;
     return decoded;
   } catch (error) {
     console.error('Invalid token', error);
-    return null;
+    return null; 
   }
-}
+};
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers['authorization']?.split(' ')[1]; 
+
+  if (!token) {
+    return res.status(403).json({ message: 'No token provided' });
+  }
+
+  const decoded = getUser(token); 
+
+  if (!decoded) {
+    return res.status(403).json({ message: 'Invalid or expired token' });
+  }
+
+  req.user = decoded;
+
+  next(); 
+};
+
 
 export const signToken = (username: string, email: string, _id: unknown) => {
   const payload = { username, email, _id };
   const secretKey = process.env.JWT_SECRET_KEY || '';
 
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' }); 
 };
